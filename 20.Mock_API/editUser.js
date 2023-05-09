cd46d31ed252e4bce31525857b7f072120173701
Nazim057 <namazovnazim057@gmail.com>
2023-05-09 15:37:09 -0700

    20.Mock_API

---
+++ 20.Mock_API/editUser.js
@@ -0,0 +1,25 @@
+const editedUser = JSON.parse(localStorage.getItem("editedUser"));
+
+const nameInput = document.getElementById("exampleInputName1");
+const emailInput = document.getElementById("exampleInputEmail1");
+
+nameInput.value = editedUser.name;
+emailInput.value = editedUser.email;
+
+const form = document.querySelector("form");
+const editBtn = document.querySelector(".editBtn");
+
+editBtn.addEventListener("click", async (event) => {
+  event.preventDefault();
+
+  const name = nameInput.value;
+  const email = emailInput.value;
+
+  try {
+    await axios.put(`http://localhost:5700/data/${editedUser.id}`, { name, email });
+    localStorage.removeItem("editedUser");
+    window.location.href = "index.html";
+  } catch (error) {
+    console.error(error);
+  }
+});
