675a3fc617858f7a4690640939a13e896529c618
Nazim057 <namazovnazim057@gmail.com>
2023-05-15 03:08:05 -0700

    id-class

--- 21.LoopStudioLandingPage/script.js
+++ 21.LoopStudioLandingPage/script.js
@@ -1,5 +1,5 @@
-var iconHamburger = document.getElementById("iconHamburger");
-var navbarr = document.getElementById("navbarr");
+let iconHamburger = document.querySelector(".iconHamburger")
+let navbarr = document.querySelector(".navbarr");
 
 iconHamburger.addEventListener("click", function () {
   navbarr.classList.toggle("navbarrr");
