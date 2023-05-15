823b66e8594f3e4da34d41cb25799932501bf2a7
Nazim057 <namazovnazim057@gmail.com>
2023-05-15 02:41:13 -0700

    LoopStudio Landing Page

---
+++ 21.LoopStudioLandingPage/script.js
@@ -0,0 +1,6 @@
+var iconHamburger = document.getElementById("iconHamburger");
+var navbarr = document.getElementById("navbarr");
+
+iconHamburger.addEventListener("click", function () {
+  navbarr.classList.toggle("navbarrr");
+});
